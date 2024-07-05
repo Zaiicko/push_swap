5324f84356c3718d6aecf06f3480ca07a8c13445
Mehdi <zaiicko7@proton.me>
2024-07-05 02:56:57 +0200

    Update : Add double list struct

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/22 23:28:23 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/06/23 00:40:16 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/07/05 02:50:24 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 # define PUSH_SWAP_H
 
 # include "../libft/inc/libft.h"
+
+typedef struct s_node
+{
+	int				data;
+	struct s_node	*next;
+	struct s_node	*prev;
+}	t_node;
 
 #endif
