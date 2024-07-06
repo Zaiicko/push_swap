535eab6958c3e5183e2da7f4aae9938a6d1a97af
Mehdi <zaiicko7@proton.me>
2024-07-06 20:58:14 +0200

    Update : Norm

--- srcs/node_utils.c
+++ srcs/node_utils.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:51:35 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/07/06 19:23:36 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/07/06 20:55:12 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 
 t_node	*ft_newnode(int data)
 {
-	t_node	*new = (t_node *)malloc(sizeof(t_node));
+	t_node	*new;
+
+	new = (t_node *)malloc(sizeof(t_node));
 	if (!new)
 		return (NULL);
 	new->data = data;
