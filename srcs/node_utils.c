4843009cf9b59a4f545dd1a47cab1221e92c9d6f
Mehdi <zaiicko7@proton.me>
2024-07-06 19:26:07 +0200

    Update : Put the correct syntax for malloc casting

--- srcs/node_utils.c
+++ srcs/node_utils.c
@@ -6,7 +6,7 @@
 /*   By: zaiicko <meskrabe@student.s19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 18:51:35 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/07/06 19:14:40 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/07/06 19:23:36 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 t_node	*ft_newnode(int data)
 {
-	t_node	*new = (t_node)malloc(sizeof(t_node));
+	t_node	*new = (t_node *)malloc(sizeof(t_node));
 	if (!new)
 		return (NULL);
 	new->data = data;
